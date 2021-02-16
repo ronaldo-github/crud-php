1911a2cb85cf6bc95d2b6d3da9e1453dbb266d3a
unknown <ronaldofonseca123@gmail.com>
2021-02-16 09:59:38 -0300

    script_banco

---
+++ script_banco.sql
@@ -0,0 +1,40 @@
+/*
+SQLyog Community Edition- MySQL GUI v6.54
+MySQL - 5.1.33-community : Database - teste
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`teste` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `teste`;
+
+/*Table structure for table `pessoa` */
+
+DROP TABLE IF EXISTS `pessoa`;
+
+CREATE TABLE `pessoa` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(50) DEFAULT NULL,
+  `cpf` int(15) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+/*Table structure for table `telefone` */
+
+DROP TABLE IF EXISTS `telefone`;
+
+CREATE TABLE `telefone` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `telefone` varchar(15) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
